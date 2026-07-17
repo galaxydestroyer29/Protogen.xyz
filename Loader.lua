633749ca904e707f712bdf02aff3823ca8b0f085
galaxydestroyer29 <galaxydestroyed96@gmail.com>
2026-07-17 22:09:26 +0200

    Updated

--- Loader.lua
+++ Loader.lua
@@ -1,15 +1,13 @@
--- === protogen.xyz Frontend Loader ===
-print("protogen.xyz frontend loader started...")
 
-local FrontendURL = "https://raw.githubusercontent.com/galaxydestroyer29/Protogen.xyz/refs/heads/main/UI.lua" 
+-- === protogen.xyz Frontend Loader ===
+print("Frontend busted in my ass successfully!")
 
 local success, err = pcall(function()
-    loadstring(game:HttpGet(FrontendURL, true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/galaxydestroyer29/Protogen.xyz/refs/heads/main/UI.lua", true))()
 end)
 
 if success then
-    print("Frontend busted in my ass successfully!")
     print("protogen.xyz ready.")
 else
-    warn(" Frontend didnt bust. Not horny, send error to dev: " .. tostring(err))
+    warn("Frontend didnt bust. Not horny, send error to dev: " .. tostring(err))
 end
