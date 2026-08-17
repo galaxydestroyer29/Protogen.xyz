9018a44bf0d90e3a51d4c0b535b7ae7fcd5ed8e0
galaxydestroyer29 <galaxydestroyed96@gmail.com>
2026-08-18 01:18:44 +0200

    Updated

--- Loader.lua
+++ Loader.lua
@@ -1,12 +1,2 @@
 
-local rawUrl = "https://raw.githubusercontent.com/galaxydestroyer29/Protogen.xyz/refs/heads/main/67UIsupercool.lua"
-
-local cacheBypassUrl = rawUrl .. "?t=" .. tostring(tick())
-
-local success, result = pcall(function()
-    return loadstring(game:HttpGet(cacheBypassUrl))()
-end)
-
-if not success then
-    warn("Failed to load script: " .. tostring(result))
-end
+loadstring(game:HttpGet(https://raw.githubusercontent.com/galaxydestroyer29/Protogen.xyz/refs/heads/main/67UIsupercool.lua))
