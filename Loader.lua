969fbff7ac19fe1d784d29bfcc10e3f947125b59
galaxydestroyer29 <galaxydestroyed96@gmail.com>
2026-08-18 01:21:29 +0200

    67

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 
-loadstring(game:HttpGet(https://raw.githubusercontent.com/galaxydestroyer29/Protogen.xyz/refs/heads/main/67UIsupercool.lua))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/galaxydestroyer29/Protogen.xyz/refs/heads/main/67UIsupercool.lua"))
